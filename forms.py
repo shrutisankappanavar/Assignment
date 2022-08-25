e61a7a777ef7e839b49ad6bc83dd2d4d42318a40
[shruti sankappanavar] <[shruti.sankappanavar@calsoftinc.com]>
2022-08-25 15:26:54 +0530

    test_files2

---
+++ forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control',
+                                                             }))
+    password = forms.CharField(max_length=50,
+                               required=True,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                 'class': 'form-control',
+                                                                 'data-toggle': 'password',
+                                                                 'id': 'password',
+                                                                 'name': 'password',
+                                                                 }))
+    remember_me = forms.BooleanField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'remember_me']
